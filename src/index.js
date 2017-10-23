b043519a36254c955c925c37ffd277a5a111c603
Tom Chen <developer@tomchentw.com>
2017-10-23 15:50:46 +0800

    feat(index.js): add `options.disabled` and `options.useYarn`
    * Closes #3

--- src/index.js
+++ src/index.js
@@ -1,87 +1,87 @@
-import {
-  exec as nodeExec,
-} from "child_process";
+import { exec as nativeExec } from "child_process";
+import promisify from "util.promisify";
+import webpack from "webpack";
+import MemoryFS from "memory-fs";
+import validateNpmPackageName from "validate-npm-package-name";
 
-import {
-  default as thenify,
-} from "thenify";
+async function findMissingModuleList(webpackConfig) {
+  const webpackCompiler = webpack(webpackConfig);
+  webpackCompiler.outputFileSystem = new MemoryFS();
 
-import {
-  default as webpack,
-} from "webpack";
+  const webpackStats = await promisify(
+    webpackCompiler.run.bind(webpackCompiler)
+  )();
+  const { errors } = webpackStats.toJson();
 
-import {
-  default as MemoryFS,
-} from "memory-fs";
+  const rawModuleList = errors
+    .map(error => {
+      if (
+        // v3
+        /Module not found: Error: Can't resolve '(\S+)' in/.test(error)
+      ) {
+        return RegExp.$1;
+      } else if (
+        // v1
+        /Module not found: Error: Cannot resolve module '(\S+)' in/.test(error)
+      ) {
+        return RegExp.$1;
+      } else {
+        return undefined;
+      }
+    })
+    .filter(it => !!it)
+    .map(it => it.split(`/`)[0]);
 
-import {
-  default as validateNpmPackageName,
-} from "validate-npm-package-name";
+  const areAllModuleValid = rawModuleList.every(
+    it => validateNpmPackageName(it).validForNewPackages
+  );
 
-const exec = thenify(nodeExec);
+  if (!areAllModuleValid) {
+    throw new Error(`Unexpected module name`);
+  }
+  return rawModuleList;
+}
+
+async function installModuleList(moduleList, plugin) {
+  const command = plugin.options.useYarn
+    ? `yarn add ${moduleList.join(` `)}`
+    : `npm install ${moduleList.join(` `)}`;
+
+  await new Promise((resolve, reject) => {
+    const cp = nativeExec(`${command} ${moduleList.join(` `)}`);
+    cp.once("close", resolve);
+    cp.once("error", reject);
+  });
+}
+
+async function runCompiler(compiler, plugin) {
+  if (plugin.options.disabled) {
+    return;
+  }
+  const { options } = compiler;
+  const moduleList = await findMissingModuleList({
+    ...options,
+    plugins: options.plugins.filter(it => it !== plugin)
+  });
+  await installModuleList(moduleList, plugin);
+}
 
 export class NpmAutoInstallWebpackPlugin {
   constructor(options = {}) {
     this.options = {
       ...options,
-      autoInstall: options.autoInstall !== false,
+      disabled: options.disabled === true
     };
   }
 
   apply(compiler) {
     const runCompilerThenCallback = (...args) => {
-      const callback = args[args.length - 1];
-      this._runCompiler(compiler)
-        .then(() => callback(), callback);
+      const done = args[args.length - 1];
+      runCompiler(compiler, this).then(done, done);
     };
 
     compiler.plugin(`run`, runCompilerThenCallback);
     compiler.plugin(`watch-run`, runCompilerThenCallback);
-  }
-
-  async _runCompiler(compiler) {
-    const { options } = compiler;
-    const { plugins } = options;
-
-    const moduleList = await this._findMissingModuleList({
-      ...options,
-      plugins: plugins.filter(it => it.constructor !== NpmAutoInstallWebpackPlugin),
-    });
-
-    if (this.options.autoInstall) {
-      await this._installModuleList(moduleList);
-    }
-  }
-
-  async _findMissingModuleList(webpackConfig) {
-    const webpackCompiler = webpack(webpackConfig);
-    webpackCompiler.outputFileSystem = new MemoryFS();
-
-    const webpackStats = await thenify(::webpackCompiler.run)();
-    const { errors } = webpackStats.toJson();
-
-    const rawModuleList = errors
-      .map(error => {
-        if (/Module not found: Error: Cannot resolve module '(\S+)' in/.test(error)) {
-          return RegExp.$1;
-        } else {
-          return undefined;
-        }
-      })
-      .filter(it => !!it)
-      .map(it => it.split(`/`)[0]);
-
-    const areAllModuleValid = rawModuleList
-      .every(it => validateNpmPackageName(it).validForNewPackages);
-
-    if (!areAllModuleValid) {
-      throw new Error(`Unexpected module name`);
-    }
-    return rawModuleList;
-  }
-
-  async _installModuleList(moduleList) {
-    return exec(`npm install ${moduleList.join(` `)}`);
   }
 }
 
